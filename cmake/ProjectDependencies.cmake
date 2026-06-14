297792c3c724fb558d750e6245fab586cf5efb0f
casey.johnson <onnesh1@gmail.com>
2026-06-14 14:17:40 -0700

    Fixed a project dependency issue with spdlg and fmt by directly adding fmt to the project
    Added a ReadMe for building

--- cmake/ProjectDependencies.cmake
+++ cmake/ProjectDependencies.cmake
@@ -7,6 +7,7 @@ CPMAddPackage("gh:g-truc/glm#1.0.3")
 CPMAddPackage("gh:catchorg/Catch2#v3.15.0")
 CPMAddPackage("gh:gabime/spdlog#v1.17.0")
 CPMAddPackage("gh:GPUOpen-LibrariesAndSDKs/VulkanMemoryAllocator#v3.2.1")
+CPMAddPackage("gh:fmtlib/fmt#11.0.2")
 
 find_package(Vulkan REQUIRED)
 get_target_property(VULKAN_LIB_LOCATION Vulkan::Vulkan IMPORTED_LOCATION)
