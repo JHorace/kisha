18ec8f7e0b2134b0a4d7dc70cfe0e600c77aa89f
Russell Johnson <encrypteddata0@gmail.com>
2026-06-13 17:04:04 -0700

    Added Cmake Package Manager and cleaned up the CMake codebase a little (#1)
    * Added Cmake Package Manager and cleaned up the CMake codebase a little
    
    * Removed comment

--- cmake/ProjectOptions.cmake
+++ cmake/ProjectOptions.cmake
@@ -1,7 +1,3 @@
 include_guard(GLOBAL)
 
-set(CMAKE_CXX_STANDARD 23)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
-
 option(KISHA_BUILD_ENGINE "Configure the kisha-engine subtree." ON)
